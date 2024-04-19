df4fb26dea927adcc4dab2bb7e94f398c733b277
Vandana Ganwal <vandanaganwal12@gmail.com>
2024-04-19 19:21:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+
+const newItem = document.getElementById("inputItem");
+const item = document.getElementById("taskList");
+const addItem = document.getElementById("addItem");
+
+addItem.addEventListener('click', () => {
+    if(newItem.value != "" ) {
+        const task = newItem.value;
+        const listTask = document.createElement("li");
+        listTask.innerHTML = `
+            ${task} 
+            <button class="clearItem"> Clear </button>
+            `;
+        item.appendChild(listTask);
+        // newItem.value = "";
+    }
+})
+
+
+
+item.addEventListener("click", (e) => {
+    if(e.target.classList.contains("clearItem")) {
+        const listItem = e.target.parentElement;
+        item.removeChild(listItem);
+    }
+})
+
+
+
+
+
+
+
+
+
+
